d2c80bd3608b7d9aac881df57a0d98e99f3a8598
Alberto Bonacina <bonacina.alberto@gmail.com>
2021-05-28 07:42:57 +0200

    add: First version with some data in the listView

---
+++ lib/models/contact.dart
@@ -0,0 +1,21 @@
+class Contact {
+  final String name;
+  final String surname;
+  final String phone;
+  final String email;
+  final String address;
+  final String city;
+  final String username;
+  final String prefix;
+
+  Contact({
+    required this.name,
+    required this.surname,
+    required this.phone,
+    required this.email,
+    required this.address,
+    required this.city,
+    required this.username,
+    required this.prefix,
+  });
+}
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,17 +1,21 @@
 import 'package:flutter/material.dart';
 
+import 'package:faker/faker.dart';
+import '../ui/contact_list.dart';
+import '../models/contact.dart';
+
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Simple Flutter Timetable',
+      title: 'Simple Flutter Contact List',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         brightness: Brightness.dark,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Simple Flutter Timetable'),
+      home: MyHomePage(title: 'Simple Flutter Contact List'),
     );
   }
 }
@@ -25,7 +29,25 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  final int _contactListNumberPeople = 25;
+  var faker = new Faker();
 
+  Future<List<Contact>> _createContactList() async {
+    List<Contact> _list = List<Contact>.empty(growable: true);
+    for (int i = 0; i < _contactListNumberPeople; i++) {
+      Contact thisPerson = new Contact(
+          name: faker.person.firstName(),
+          surname: faker.person.lastName(),
+          phone: '999999',
+          email: faker.internet.email(),
+          address: faker.address.streetAddress(),
+          city: faker.address.city(),
+          username: faker.internet.userName(),
+          prefix: faker.person.prefix());
+      _list.add(thisPerson);
+    }
+    return _list;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -34,7 +56,21 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: new Container(
-        child: new Center(),
+        child: new FutureBuilder<List<Contact>>(
+          future: _createContactList(),
+          builder: (context, snapshot) {
+            if (!snapshot.hasData) {
+              return new Center(
+                child: new CircularProgressIndicator(),
+              );
+            } else {
+              List<Contact> myContactList = snapshot.data ?? [];
+              return new ContactList(
+                contacts: myContactList,
+              );
+            }
+          },
+        ),
       ),
     );
   }
